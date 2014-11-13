54e613c0f8054304834c7874251e860c050e0a37
UngarMax <robriplopez@gmail.com>
2014-11-13 19:25:05 +0100

    Initial commit
    Source code upload. It will only launch it in case the game has been
    installed correctly (and the game's location exists in Windows'
    Registry).

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Client
+{
+    using System;
+    using System.Diagnostics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log("Initializing...");
+            Console.Title = "DayZEpoch Launcher";
+            Log("ArmA2 location: " + Utils.Arma2Location());
+            Log("Creating ProcessInfo...");
+            ProcessStartInfo Game = new ProcessStartInfo();
+            Game.UseShellExecute = true;
+            Game.Verb = "runas";
+            Game.WorkingDirectory = Utils.Arma2Location();
+            Game.FileName = Utils.Arma2Location() + "\\Expansion\\beta\\" + "arma2oa.exe";
+            Log("ArmA2 OA location: " + Game.FileName);
+            Log("Getting ini content...");
+            string serverIP, serverPort;
+            Utils.GetServer(out serverIP, out serverPort);
+            Log("Got " + serverIP + ":" + serverPort + " from file");
+            Game.Arguments = Utils.GetArguments();
+            Log("Starting the game...");
+            Log("Connecting to [" + serverIP + ":" + serverPort + "]");
+            Log("Closing...");
+            Process.Start(Game);
+            Environment.Exit(0x0);
+        }
+
+        private static void Log(string Input)
+        {
+            Input = DateTime.Now.ToString("[dd-MM-yyyy @ HH:mm:ss]") + " - " + Input;
+            Console.WriteLine(Input);
+            return;
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,74 @@
+﻿namespace Client
+{
+    using System;
+    using Microsoft.Win32;
+
+    public class Utils
+    {
+        public static string Arma2Location()
+        {            
+            RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Bohemia Interactive Studio\ArmA 2 OA");
+
+            if (key != null) return key.GetValue("MAIN").ToString();
+            else
+            {
+                key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Bohemia Interactive Studio\ArmA 2 OA");
+                if (key != null) return key.GetValue("MAIN").ToString();
+                else return string.Empty;
+            }
+        }
+
+        public static string GetArguments()
+        {
+            string Arguments, serverIP, serverPort;
+            GetServer(out serverIP, out serverPort);
+
+            if (windowedMode())
+            {
+                if (GetPassword() == string.Empty)
+                {
+                    Arguments = "-window -skipIntro -mod=@DayZ_Epoch -noSplash -noFilePatching -world=empty -connect=" + serverIP + " -port=" + serverPort + " \"-mod=" + Arma2Location() + ";expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ_Epoch\"";
+                }
+                else
+                {
+                    Arguments = "-window -skipIntro -mod=@DayZ_Epoch -noSplash -noFilePatching -world=empty -connect=" + serverIP + " -port=" + serverPort + " -password=" + GetPassword() + " \"-mod=" + Arma2Location() + ";expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ_Epoch\"";
+                }
+            }
+            else
+            {
+                if (GetPassword() == string.Empty)
+                {
+                    Arguments = "-skipIntro -mod=@DayZ_Epoch -noSplash -noFilePatching -world=empty -connect=" + serverIP + " -port=" + serverPort + " \"-mod=" + Arma2Location() + ";expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ_Epoch\"";
+                }
+                else
+                {
+                    Arguments = "-skipIntro -mod=@DayZ_Epoch -noSplash -noFilePatching -world=empty -connect=" + serverIP + " -port=" + serverPort + " -password=" + GetPassword() + " \"-mod=" + Arma2Location() + ";expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ_Epoch\"";
+                }
+            }
+
+            return Arguments;
+        }
+
+        public static bool windowedMode()
+        {
+            string[] Lines = System.IO.File.ReadAllLines(Environment.CurrentDirectory + @"\Client.ini");
+            if (Lines[0].Replace("Windowed=", string.Empty) == "0") return false;
+            else return true;
+        }
+
+        public static void GetServer(out string serverIP, out string serverPort)
+        {
+            string[] Lines = System.IO.File.ReadAllLines(Environment.CurrentDirectory + @"\Client.ini");
+            string serverInfo = Lines[1].Replace("Server=", string.Empty);
+            serverIP = serverInfo.Split(':')[0];
+            serverPort = serverInfo.Split(':')[1];
+            return;
+        }
+
+        public static string GetPassword()
+        {
+            string[] Lines = System.IO.File.ReadAllLines(Environment.CurrentDirectory + @"\Client.ini");
+            return Lines[2].Replace("Password=", string.Empty);
+        }
+    }
+}
